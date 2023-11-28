1a98c4f32b75bcf274a1818c3023bc017f1d103d
yasinsametkaraca <80101882+yasinsametkaraca@users.noreply.github.com>
2023-11-28 15:27:21 +0300

    feat: create abstract model

--- core/models.py
+++ core/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 
-class GeneralSetting(models.Model):
+class AbstractBaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, verbose_name='Created At')
+    updated_at = models.DateTimeField(auto_now=True, blank=True, verbose_name='Updated At')
+
+    class Meta:
+        abstract = True  # abstract model means that this model will not be created in database
+
+
+class GeneralSetting(AbstractBaseModel):
     name = models.CharField(default='', max_length=250, blank=True, help_text='Name of the setting')
     description = models.CharField(default='', max_length=250)
     parameters = models.TextField(default='', max_length=750)
-    created_at = models.DateTimeField(auto_now_add=True, blank=True)
-    updated_at = models.DateTimeField(auto_now=True, blank=True)
 
     def __str__(self):
         return f'General Setting: {self.name}'
@@ -17,12 +23,10 @@ class GeneralSetting(models.Model):
         ordering = ('name', )
 
 
-class ImageSetting(models.Model):
+class ImageSetting(AbstractBaseModel):
     name = models.CharField(default='', max_length=250, blank=True, help_text='Name of the setting', verbose_name='Name')
     description = models.CharField(default='', max_length=250, blank=True, verbose_name='Description')
     file = models.ImageField(default='', blank=True, null=True, verbose_name='Image', upload_to='images/')
-    created_at = models.DateTimeField(auto_now_add=True, blank=True)
-    updated_at = models.DateTimeField(auto_now=True, blank=True)
 
     def __str__(self):
         return f'Image Setting: {self.name}'
